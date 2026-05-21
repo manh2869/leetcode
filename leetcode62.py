b61923fe1604c54336ff9f363d69209db1da8da2
manh2869 <mank159bmt@gmail.com>
2026-05-21 10:09:48 +0700

    21/5/2026

--- leetcode62.py
+++ leetcode62.py
@@ -1,9 +1,16 @@
 def uniquePaths(m, n):
-    def bt(i, j):
-        if (i == m - 1) and (j == n - 1):
-            return 1
-        if i == m or j == n:
-            return 0
-        return bt(i + 1, j) + bt(i, j + 1)
 
-    return bt(0, 0)
+    res = [[0 for _ in range(n)] for _ in range(m)]
+    for i in range(m - 1, -1, -1):
+        res[i][n - 1] = 1
+    for i in range(n - 1, -1, -1):
+        res[m - 1][i] = 1
+
+    for i in range(m - 2, -1, -1):
+        for j in range(n - 2, -1, -1):
+            res[i][j] = res[i + 1][j] + res[i][j + 1]
+
+            # print(f"{i}   {j}    {res[i-1][j]}    {res[i][j-1]}")
+    return res[0][0]
+
+uniquePaths(3, 7)
