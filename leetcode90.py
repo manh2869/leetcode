e01c38533227fc64d426bf6f416d04875de87d5e
manh2869 <mank159bmt@gmail.com>
2026-06-04 14:08:32 +0700

    4/6/2026

---
+++ leetcode90.py
@@ -0,0 +1,20 @@
+def subsetsWithDup(nums):
+    result = []
+    curr = []
+    nums = sorted(nums)
+
+    def dfs(i):
+        if i >= len(nums):
+            result.append(curr[:])
+            return
+        curr.append(nums[i])
+        dfs(i + 1)
+        curr.pop()
+        dfs(i + 1)
+
+    dfs(0)
+    print(result)
+    return result
+?? bai lozz nay
+
+subsetsWithDup([3, 1, 2])
