fb65e6f51e68a137d029a86440797708278751d6
manh2869 <mank159bmt@gmail.com>
2026-06-12 22:24:48 +0700

    12/6/2026

--- leetcode_c++/leetcode119.cpp
+++ leetcode_c++/leetcode119.cpp
@@ -2,20 +2,31 @@
 #include <vector>
 using namespace std;
 
-
 vector<int> getRow(int rowIndex)
 {
-    vector<vector<int>> a;
-    vector<int> b;
+    vector<vector<int>> a = {{1}};
     for (int i = 0; i < rowIndex; i++)
     {
-        cout << i << endl;
+        vector<int> curr = {1};
+        vector<int> prev = a.back();
+        for (int j = 0; j < prev.size() - 1; j++)
+        {
+            curr.push_back(prev[j] + prev[j + 1]);
+        }
+        curr.push_back(1);
+        a.push_back(curr);
     }
-    return b;
+
+    return a.back();
 }
 
 int main()
 {
-    getRow(6);
+    vector<int> a = getRow(6);
+    for (int x : a)
+    {
+        cout << x << endl;
+    }
+
     cout << "vai ca loz" << endl;
 }
--- leetcode_python/leetcode118.py
+++ leetcode_python/leetcode118.py
@@ -12,3 +12,4 @@ def generate(numRows):
 
 
 generate(-1)
+ 
