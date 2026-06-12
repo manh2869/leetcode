55e0be5f5bac52cce553e12ccebf067d67139d25
manh2869 <mank159bmt@gmail.com>
2026-06-12 22:06:55 +0700

    12/6/2026

--- leetcode_c++/leetcode119.cpp
+++ leetcode_c++/leetcode119.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+
+vector<int> getRow(int rowIndex)
+{
+    vector<vector<int>> a;
+    vector<int> b;
+    for (int i = 0; i < rowIndex; i++)
+    {
+        cout << i << endl;
+    }
+    return b;
+}
+
 int main()
 {
+    getRow(6);
     cout << "vai ca loz" << endl;
 }
