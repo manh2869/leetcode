ecce80fc2ce2d843434c1d99948e7d92c246d58d
manh2869 <mank159bmt@gmail.com>
2026-06-13 10:24:05 +0700

    13/6/2026

---
+++ leetcode_c++/leetcode120.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int minimumTotal(vector<vector<int>> &triangle)
+{
+    // for ()
+
+    cout << "con di me may" << endl;
+    return 0;
+}
+
+int main()
+{
+    vector<vector<int>> triangle = {{2},
+                                    {3, 4},
+                                    {6, 5, 7},
+                                    {4, 1, 8, 3}};
+    minimumTotal(triangle);
+}
