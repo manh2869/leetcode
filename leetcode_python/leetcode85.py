9aa6e81f2acbd65ac0b477cedce47cb90e93a2f1
manh2869 <mank159bmt@gmail.com>
2026-09-15 17:43:39 +0700

    15/9/2026

--- leetcode_python/leetcode85.py
+++ leetcode_python/leetcode85.py
@@ -2,8 +2,8 @@ def maximalRectangle(heights):
     max_a = 0
     stack = []
     for i in range(len(heights)):
-        while stack and heights[i] > heights[stack[-1]]:
-            h = stack.pop(-1)
+        while stack and heights[i] < heights[stack[-1]]:
+            h = stack.pop()
             if stack:
                 w = i - stack[-1] - 1
             else:
@@ -24,17 +24,29 @@ def maximalRectangle(heights):
         max_a = max(max_a, h * w)
     return max_a
 
+# print(maximalRectangle([3, 1, 3, 2, 3]))
 
 def add(matrix):
     mat = []
     for i in matrix:
+        m = []
         for j in i:
-            mat.append(int(j))
-    return mat
+            m.append(int(j))
+        mat.append(m)
+    for i in range(1, len(mat)):
+        for j in range(len(mat[0]) ):
+            if mat[i][j] == 0:
+                continue
+            mat[i][j] = mat[i - 1][j] + mat[i][j]
+    max_area = 0
+    for i in mat:
+        max_area = max(maximalRectangle(i), max_area)
+        print(i)
+    return max_area
 
 
 a = [
-    ["1", "0", "1", "0", "0"],
+    ["1", "0", "1", "0", "1"],
     ["1", "0", "1", "1", "1"],
     ["1", "1", "1", "1", "1"],
     ["1", "0", "0", "1", "0"],
