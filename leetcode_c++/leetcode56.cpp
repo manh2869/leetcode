ff7466668a62e5fc5c793d9b4fd098133afb3f48
manh2869 <mank159bmt@gmail.com>
2026-09-16 13:08:15 +0700

    16/9/2026

---
+++ leetcode_c++/leetcode56.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<vector<int>> merge(vector<vector<int>> &intervals)
+{
+    vector<vector<int>> a;
+    for (vector<int> &x : intervals)
+        sort(x.begin(), x.end());
+    sort(intervals.begin(), intervals.end());
+
+    for (int i = 0; i < intervals.size(); i++)
+    {
+        // for (int z : x)
+        if (x)
+    }
+    return intervals;
+}
+int main()
+{
+    vector<vector<int>> a = {
+        {4, 7},
+        {4, 1},
+    };
+    merge(a);
+}
