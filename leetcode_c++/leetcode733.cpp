f87733804c3e01e1c3bb3d365d3a5f30e472e572
manh2869 <mank159bmt@gmail.com>
2026-09-21 19:42:31 +0700

    21/9/2026

--- leetcode_c++/leetcode733.cpp
+++ leetcode_c++/leetcode733.cpp
@@ -1,43 +1,38 @@
 #include <iostream>
 #include <vector>
-#include <bitset>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
-
-vector<vector<int>> floodFill(vector<vector<int>> &image, int x, int y, int color)
+vector<vector<int>> bfs(vector<vector<int>> &image, int x, int y, int orr, int color)
 {
-    if (image[x][y] == 0)
+    if (x >= image.size() or x < 0 or y >= image[0].size() or y < 0)
+        return image;
+    // need one if
+    if (image[x][y] != orr)
         return image;
     image[x][y] = color;
-    if (x >= image[0].size() or x < 0)
-    {
-        return image;
-    }
-    if (y >= image.size() or y < 0)
-    {
-        return image;
-    }
-    floodFill(image, x, y + 1, color);
-    floodFill(image, x + 1, y, color);
-    floodFill(image, x, y - 1, color);
-    floodFill(image, x - 1, y, color);
-    cout << x << y << endl;
-    // for (vector<int> x : image)
-    // {
-    //     for (int z : x)
-    //     {
-    //         cout << z;
-    //     }
-    //     cout << endl;
-    // }
-
+    bfs(image, x + 1, y, orr, color);
+    bfs(image, x, y - 1, orr, color);
+    bfs(image, x - 1, y, orr, color);
+    bfs(image, x, y + 1, orr, color);
     return image;
+}
+vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int color)
+{
+    return bfs(image, sr, sc, image[sr][sc], color);
 }
 int main()
 {
     vector<vector<int>> image = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
-    //  vector<vector<int*>> t=&image;
     int sr = 1, sc = 1, color = 2;
     floodFill(image, sr, sc, color);
+    for (vector<int> x : image)
+    {
+        for (int z : x)
+        {
+            cout << z;
+        }
+        cout << endl;
+    }
 }
