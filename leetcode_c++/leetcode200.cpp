42ae672554bdb78ff6036357abefc8d0041501fd
manh2869 <mank159bmt@gmail.com>
2026-09-23 20:30:12 +0700

    23/9/2026

--- leetcode_c++/leetcode200.cpp
+++ leetcode_c++/leetcode200.cpp
@@ -2,18 +2,32 @@
 #include <queue>
 #include <vector>
 using namespace std;
+int bfs(vector<vector<char>> &grid, vector<vector<int>> check /*test*/, int x, int y)
+{
 
+    if (grid[x][y] == '0' || check[x][y] == 1 || x < 0 || y < 0 || x >= grid.size() || y >= grid[0].size())
+    {
+        return 0;
+    }
+    bfs(grid, check, x + 1, y);
+    bfs(grid, check, x, y - 1);
+    return 1;
+}
 int numIslands(vector<vector<char>> &grid)
 {
-    for (vector<char> a : grid)
+    vector<vector<int>> check = {
+        grid.size(),
+        vector<int>(grid[0].size(), 0)};
+    int sum = 0;
+    for (int i = 0; i < grid.size(); i++)
     {
-        for (char x : a)
+        for (int j = 0; j < grid[0].size(); j++)
         {
-            cout << x;
+            sum += bfs(grid, check, i, j);
         }
-        cout << endl;
     }
-    return 0;
+
+    return sum;
 }
 int main()
 {
@@ -22,6 +36,6 @@ int main()
         {'1', '1', '0', '1', '0'},
         {'1', '1', '0', '0', '0'},
         {'0', '0', '0', '0', '0'}};
-    numIslands(grid);
+    cout << numIslands(grid) << endl;
     return 0;
 }
