afac945776ba74199dbf16266cba5720cc4f64b2
manh2869 <mank159bmt@gmail.com>
2026-09-23 20:56:33 +0700

    23/9/2026

--- leetcode_c++/leetcode200.cpp
+++ leetcode_c++/leetcode200.cpp
@@ -2,27 +2,34 @@
 #include <queue>
 #include <vector>
 using namespace std;
-int bfs(vector<vector<char>> &grid, vector<vector<int>> check /*test*/, int x, int y)
+int bfs(vector<vector<char>> &grid, vector<vector<int>> &check /*test*/, int x, int y)
 {
 
-    if (grid[x][y] == '0' || check[x][y] == 1 || x < 0 || y < 0 || x >= grid.size() || y >= grid[0].size())
+    if (x < 0 || y < 0 || x >= grid.size() || y >= grid[0].size() || check[x][y] == 1 || grid[x][y] == '0')
     {
         return 0;
     }
+    check[x][y] = 1;
     bfs(grid, check, x + 1, y);
+    bfs(grid, check, x - 1, y);
+    bfs(grid, check, x, y + 1);
     bfs(grid, check, x, y - 1);
     return 1;
 }
 int numIslands(vector<vector<char>> &grid)
 {
-    vector<vector<int>> check = {
+    // vector<vector<int>> check = (grid.size(),
+    //                              vector<int>(grid[0].size(), 0));
+    vector<vector<int>> check(
         grid.size(),
-        vector<int>(grid[0].size(), 0)};
+        vector<int>(grid[0].size(), 0));
     int sum = 0;
     for (int i = 0; i < grid.size(); i++)
     {
         for (int j = 0; j < grid[0].size(); j++)
         {
+            if (grid[i][j] == '0')
+                continue;
             sum += bfs(grid, check, i, j);
         }
     }
