63d66aa584f689d9e44e21f886cab7baf1d1baf4
manh2869 <mank159bmt@gmail.com>
2026-09-24 20:14:57 +0700

    24/9/2026

--- leetcode_c++/leetcode1926.cpp
+++ leetcode_c++/leetcode1926.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
+#include <algorithm>
 #include <queue>
 #include <vector>
 using namespace std;
-int min_exit(vector<vector<char>> &maze, int x, int y, int count)
+int min_exit(vector<vector<char>> &maze, vector<vector<bool>> &visited, int x, int y, int count)
 {
-    if (x < 0 || y < 0 || x >= maze.size() || y >= maze[0].size() || maze[x][y] == '+')
+    if (x < 0 || y < 0 || x >= maze.size() || y >= maze[0].size() || maze[x][y] == '+' ||
+        visited[x][y])
     {
-        return 0;
+        return 99999999;
     }
-    if ((x == 0 || y == 0 || x == maze.size() - 1 || y == maze[0].size() - 1) & maze[x][y] == '.')
+    if ((x == 0 || y == 0 || x == maze.size() - 1 || y == maze[0].size() - 1) && maze[x][y] == '.')
         return count;
-    min_exit(maze, x + 1, y, count + 1);
-    min_exit(maze, x, y + 1, count + 1);
-    min_exit(maze, x - 1, y, count + 1);
-    min_exit(maze, x, y - 1, count + 1);
-    return 0;
+    visited[x][y] = true;
+
+    int a = min_exit(maze, visited, x + 1, y, count + 1);
+    int b = min_exit(maze, visited, x, y + 1, count + 1);
+    int c = min_exit(maze, visited, x - 1, y, count + 1);
+    int d = min_exit(maze, visited, x, y - 1, count + 1);
+
+    return min({a, b, c, d});
 }
 int nearestExit(vector<vector<char>> &maze, vector<int> &entrance)
 {
-    min_exit(maze, entrance[0], entrance[1],0);
+    vector<vector<bool>> visited(
+        maze.size(),
+        vector<bool>(maze[0].size(), false));
+    min_exit(maze, visited, entrance[0], entrance[1], 0);
     return 0;
 }
 int main()
