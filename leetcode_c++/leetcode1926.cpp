0008827ab1cde1ca284a2d4e3d893780fe182505
manh2869 <mank159bmt@gmail.com>
2026-09-24 22:13:19 +0700

    24/9/2026

--- leetcode_c++/leetcode1926.cpp
+++ leetcode_c++/leetcode1926.cpp
@@ -3,38 +3,49 @@
 #include <queue>
 #include <vector>
 using namespace std;
-int min_exit(vector<vector<char>> &maze, vector<vector<bool>> &visited, int x, int y, int count)
-{
-    if (x < 0 || y < 0 || x >= maze.size() || y >= maze[0].size() || maze[x][y] == '+' ||
-        visited[x][y])
-    {
-        return 99999999;
-    }
-    if ((x == 0 || y == 0 || x == maze.size() - 1 || y == maze[0].size() - 1) && maze[x][y] == '.')
-        return count;
-    visited[x][y] = true;
 
-    int a = min_exit(maze, visited, x + 1, y, count + 1);
-    int b = min_exit(maze, visited, x, y + 1, count + 1);
-    int c = min_exit(maze, visited, x - 1, y, count + 1);
-    int d = min_exit(maze, visited, x, y - 1, count + 1);
-
-    return min({a, b, c, d});
-}
 int nearestExit(vector<vector<char>> &maze, vector<int> &entrance)
 {
-    vector<vector<bool>> visited(
+    vector<vector<int>> distance(
         maze.size(),
-        vector<bool>(maze[0].size(), false));
-    min_exit(maze, visited, entrance[0], entrance[1], 0);
-    return 0;
+        vector<int>(maze[0].size(), 0));
+    distance[entrance[0]][entrance[1]] = 1;
+    queue<pair<int, int>> q;
+    q.push({entrance[0], entrance[1]});
+    int dx[] = {1, -1, 0, 0};
+    int dy[] = {0, 0, 1, -1};
+    while (!q.empty())
+    {
+        pair<int, int> p = q.front();
+        q.pop();
+        int x = p.first;
+        int y = p.second;
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= maze.size() || ny >= maze[0].size())
+                continue;
+            if (maze[nx][ny] == '+')
+                continue;
+            if (distance[nx][ny] != 0)
+                continue;
+            distance[nx][ny] = distance[x][y] + 1;
+            if (nx == 0 || ny == 0 || nx == maze.size() - 1 || ny == maze[0].size() - 1)
+                return distance[nx][ny] - 1;
+            q.push({nx, ny});
+        }
+    }
+    return -1;
 }
 int main()
 {
     vector<vector<char>> maze = {
+        {'+', '+', '+', '+'},
+        {'.', '.', '.', '+'},
         {'+', '+', '.', '+'},
         {'.', '.', '.', '+'},
-        {'+', '+', '+', '.'}};
-    vector<int> entrance = {1, 2};
+        {'+', '+', '+', '+'}};
+    vector<int> entrance = {1, 0};
     cout << nearestExit(maze, entrance) << endl;
 }
